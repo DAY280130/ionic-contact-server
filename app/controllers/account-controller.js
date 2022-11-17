af13f544d1939d9bb36c1a6d7b1e126d6aad6fb1
DAY280130 <dimas.280130@gmail.com>
2022-11-17 07:28:27 +0700

    LOGIN

--- app/controllers/account-controller.js
+++ app/controllers/account-controller.js
@@ -1,56 +1,89 @@
+const bcrypt = require("bcrypt");
 const db = require("../models");
 const Account = db.accounts;
 
-exports.findAll = (req, res) => {
-  Account.find()
-    .then((result) => res.send(result))
-    .catch((error) => res.status(500).send({ message: error.message || "unknown error occured while getting all data" }));
-};
+// exports.findAll = (req, res) => {
+//   Account.find()
+//     .then((result) => res.send(result))
+//     .catch((error) => res.status(500).send({ message: error.message || "unknown error occured while getting all data" }));
+// };
 
-exports.find = (req, res) => {
-  const id = req.params.id;
+// exports.find = (req, res) => {
+//   const id = req.params.id;
 
-  Account.findById(id)
-    .then((result) => res.send(result))
-    .catch((error) => res.status(409).send({ message: error.message || "unknown error occured while getting data" }));
+//   Account.findById(id)
+//     .then((result) => res.send(result))
+//     .catch((error) => res.status(409).send({ message: error.message || "unknown error occured while getting data" }));
+// };
+
+exports.getToken = (req, res) => {
+  Account.findOne({ username: req.body.username })
+    .then((result) => {
+      if (!result) {
+        // username tidak ditemukan
+        res.status(404).send({ login_status: "failed" });
+      } else {
+        bcrypt
+          .compare(req.body.password, result.password)
+          .then((cryptresult) => {
+            if (cryptresult === true) {
+              res.send({
+                username: result.username,
+                token: Date.now() + result.password,
+                login_status: "success",
+              });
+            } else {
+              // password salah
+              res.status(404).send({ login_status: "failed" });
+            }
+          })
+          .catch((err) => res.status(409).send({ message: err.message || "unknown error occured while comparing password" }));
+      }
+    })
+    .catch((error) => res.status(409).send({ message: error.message || "unknown error occured while finding data" }));
 };
 
 exports.create = (req, res) => {
-  const account = new Account({
-    email: req.body.email,
-    password: req.body.password,
-  });
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const account = new Account({
+        username: req.body.username,
+        password: hash,
+      });
 
-  account
-    .save(account)
-    .then((result) => res.send(result))
-    .catch((error) => res.status(409).send({ message: error.message || "unknown error occured while inserting data" }));
-};
-
-exports.update = (req, res) => {
-  const id = req.params.id;
-
-  Account.findByIdAndUpdate(id, req.body)
-    .then((result) => {
-      if (!result) {
-        res.status(404).send({ message: "Data not found" });
-      } else {
-        res.send({ message: "Data updated successfully" });
-      }
+      account
+        .save(account)
+        .then((result) => res.send(result))
+        .catch((error) => res.status(409).send({ message: error.message || "unknown error occured while inserting data" }));
     })
-    .catch((error) => res.status(409).send({ message: error.message || "unknown error occured while updating data" }));
+    .catch((error) => res.status(409).send({ message: error || "unknown error occured while hashing password" }));
 };
 
-exports.delete = (req, res) => {
-  const id = req.params.id;
+// exports.update = (req, res) => {
+//   const id = req.params.id;
 
-  Account.findByIdAndRemove(id)
-    .then((result) => {
-      if (!result) {
-        res.status(404).send({ message: "Data not found" });
-      } else {
-        res.send({ message: "Data deleted successfully" });
-      }
-    })
-    .catch((error) => res.status(409).send({ message: error.message || "unknown error occured while deleting data" }));
-};
+//   Account.findByIdAndUpdate(id, req.body)
+//     .then((result) => {
+//       if (!result) {
+//         res.status(404).send({ message: "Data not found" });
+//       } else {
+//         res.send({ message: "Data updated successfully" });
+//       }
+//     })
+//     .catch((error) => res.status(409).send({ message: error.message || "unknown error occured while updating data" }));
+// };
+
+// exports.delete = (req, res) => {
+//   const id = req.params.id;
+
+//   Account.findByIdAndRemove(id)
+//     .then((result) => {
+//       if (!result) {
+//         res.status(404).send({ message: "Data not found" });
+//       } else {
+//         res.send({ message: "Data deleted successfully" });
+//       }
+//     })
+//     .catch((error) => res.status(409).send({ message: error.message || "unknown error occured while deleting data" }));
+// };
--- app/routes/account-routes.js
+++ app/routes/account-routes.js
@@ -2,11 +2,12 @@ module.exports = (app) => {
   const accounts = require("../controllers/account-controller");
   const router = require("express").Router();
 
-  router.get("/", accounts.findAll);
-  router.get("/:id", accounts.find);
+  // router.get("/", accounts.findAll);
+  // router.get("/:id", accounts.find);
   router.post("/", accounts.create);
-  router.put("/:id", accounts.update);
-  router.delete("/:id", accounts.delete);
+  router.post("/token", accounts.getToken);
+  // router.put("/:id", accounts.update);
+  // router.delete("/:id", accounts.delete);
 
   // Custom url (endpoint)
   app.use("/api/accounts", router);
